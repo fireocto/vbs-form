c7a9700ac99bee8ee0229f39565f2b74a1f561d7
Jordan Powell <jordancolepowell@gmail.com>
2024-06-13 10:46:49 -0500

    Initial commit to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+  const displayData = ({ name, age, bio }) => {
+    alert('You submitted: ')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <FormData
+          fields={['name', 'age', 'bio']}
+          onSubmit={}
+        );
 }
 
 export default App;
